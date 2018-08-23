74e8d99e66aafb3a078a91b842be7955dc75a4bd
eddie <eddiezhao@foxmail.com>
2018-08-23 17:44:49 +0800

    add react-router4

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Demo from './pages/Demo';
-import TodoList from './pages/TodoList';
 
 class App extends Component {
   constructor(props) {
@@ -18,7 +16,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <TodoList />
+        {this.props.children}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Demo from './pages/Demo';
+import TodoList from './pages/TodoList';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+  <BrowserRouter>
+    <App>
+        <ul>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/Demo'>Demo</Link></li>
+          <li><Link to='/TodoList'>TodoList</Link></li>
+        </ul>
+        <Route path='/Demo' component={Demo}/>
+        <Route path='/TodoList' component={TodoList}/>
+    </App>
+  </BrowserRouter>
+), document.getElementById('root'));
 registerServiceWorker();
